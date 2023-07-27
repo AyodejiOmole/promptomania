f979d9783f496eccaa4ebe4a13b858babf4dd7b4
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-27 10:36:46 +0100

    done with route to handle the entire app

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -2,6 +2,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const getGoogleVariables = ( ) : {clientId: string; clientSecret: string} => {
     const clientId = process.env.GOOGLE_ID;
@@ -23,12 +24,30 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
+    async session({ session }: {session: any}) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      });
 
+      session.user.id = sessionUser._id.toString();
+      
+      return session;
     },
-    async signIn({ profile }) {
+    async signIn({ profile }: {profile: any}) {
         try {
           await connectToDB();
+
+          const userExists = await User.findOne({
+            email: profile.email
+          })
+
+          if(!userExists) {
+            await User.create({
+              email: profile.email,
+              username: profile.name.replace(" ", "").toLowerCase(),
+              image: profile.picture
+            })
+          }
 
           return true;
         } catch (error) {
