402e861da57e3384aed821d9e1d3738d5b4b6db3
AyodejiOmole <omoledaniel13@gmail.com>
2023-08-05 16:13:07 +0100

    reference error - window - fixed

--- hooks/useLocalStorage.tsx
+++ hooks/useLocalStorage.tsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react"
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
-    let jsonValue = window.localStorage.getItem(key);
-    // if(typeof window !== 'undefined') {
-      // jsonValue = window.localStorage.getItem(key);
-    // }
+    let jsonValue;
+    // = window.localStorage.getItem(key);
+    if(typeof window !== 'undefined') {
+      jsonValue = window.localStorage.getItem(key);
+    }
     
     if (jsonValue != null) return JSON.parse(jsonValue)
 
